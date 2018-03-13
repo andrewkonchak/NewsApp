92b7010cb59b2789403d6386cb3e183446461d27
Andrew <andrew@Andrews-Mac.local>
2018-03-13 15:30:14 +0200

    no message

---
+++ NewsApp/NewsApp/Model/NewsModel.swift
@@ -0,0 +1,17 @@
+//
+//  NewsModel.swift
+//  NewsApp
+//
+//  Created by Andrew on 3/13/18.
+//  Copyright © 2018 Andrew Konchak. All rights reserved.
+//
+
+import Foundation
+
+struct News {
+    let title: String
+    let description: String
+    let source: String
+    let date: String
+    
+}
